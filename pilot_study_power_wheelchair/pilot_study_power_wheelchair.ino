ca481dd542a8301ce776e1656f510eaecf75d2c6
Yahya Bahçeci <yahyabahceciofficial@gmail.com>
2026-05-04 23:34:35 +0300

    I have completed the left turn code.

--- pilot_study_power_wheelchair/pilot_study_power_wheelchair.ino
+++ pilot_study_power_wheelchair/pilot_study_power_wheelchair.ino
@@ -4,9 +4,8 @@ const int in1 = 7;
 const int in2 = 6;
 const int in3 = 5;
 const int in4 = 4;
-const int e1 = 3; //ENB
-const int e2 = 9; //ENA
-
+const int e1 = 3; //ENB left
+const int e2 = 9; //ENA right
 
 int x_status;
 int y_status;
@@ -22,17 +21,52 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(e1, 255);
-  analogWrite(e2, 255);
-  digitalWrite(in1 , LOW);
-  digitalWrite(in2 , HIGH);
-  digitalWrite(in3 , HIGH);
-  digitalWrite(in4 , LOW);
 
   x_status = analogRead(x_axis);
   y_status = analogRead(y_axis);
-  delay(100);
 
   Serial.println(x_status);
   Serial.println(y_status); 
+
+// moving forward  
+  if (y_status < 200){
+    digitalWrite(in1 , HIGH);
+    digitalWrite(in2 , LOW);
+    digitalWrite(in3 , HIGH);
+    digitalWrite(in4 , LOW);
+
+    if (x_status < 255){
+      analogWrite(e1, x_status);
+      analogWrite(e2, 255-x_status);
+    }
+
+    else if (x_status > 768){
+      analogWrite(e1, x_status-768);
+      analogWrite(e2, 255);
+    }
+    
+    else if (x_status > 255 && x_status<768){
+      analogWrite(e1, 255);
+      analogWrite(e2, 255);
+    }
+  
+  }
+
+// moving back
+  else if (y_status > 700){
+    digitalWrite(in1 , LOW);
+    digitalWrite(in2 , HIGH);
+    digitalWrite(in3 , LOW);
+    digitalWrite(in4 , HIGH);
+    analogWrite(e1, 255);
+    analogWrite(e2, 255);
+  }
+
+  else{
+    analogWrite(e1, 0);
+    analogWrite(e2, 0);
+  }
+
+
+  
 }
